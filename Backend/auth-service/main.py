7cbf998825731b9ba4984af895a1f5d1da828a61
daanreddy21 <daanreddyalapaku2105@gmail.com>
2026-05-25 09:05:25 +0530

    Added Render deployment setup

--- Backend/auth-service/main.py
+++ Backend/auth-service/main.py
@@ -23,6 +23,14 @@ from models.trade_agreements_model import *
 from fastapi.staticfiles import StaticFiles
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"],)
 app.include_router(auth_router)
 app.include_router(document_router)
