166b090cf3779abdfdd4b41b0fffd5ee49ad45f3
daanreddy21 <daanreddyalapaku2105@gmail.com>
2026-05-25 12:51:24 +0530

    Added environment based API URLs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,37 +1,34 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
 
 export const getDocumentStats = async () => {
-    const response = await API.get(
-        "/documents/stats"
-    );
+    const response = await API.get("/documents/stats");
     return response.data;
 };
 
 export const getDocuments = async () => {
-    const response = await API.get(
-        "/documents"
-    );
+    const response = await API.get("/documents");
     return response.data;
 };
+
 export const getDocumentById = async (id) => {
-    const response = await API.get(
-        `/documents/${id}`
-    );
+    const response = await API.get(`/documents/${id}`);
     return response.data;
 };
 
 export const uploadDocument = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
+
     const response = await API.post(
         "/documents/upload",
         formData
     );
+
     return response.data;
 };
